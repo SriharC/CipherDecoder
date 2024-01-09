62dad198b3c8fe3bc2615051d59e356c72626e05
Srihar Chakravarthy <42175655+SriharC@users.noreply.github.com>
2024-01-09 01:17:26 -0500

    Update main.go
    Added more core functionality:
    
    - Added necessary algorithms:
        - Shared Secret algorithm required for Diffie Hellman Key-Exchange
        -  B, p, and g algorithms required for Diffie Hellman Key-Exchange

--- Cipher Decoder/main/main.go
+++ Cipher Decoder/main/main.go
@@ -8,18 +8,26 @@ import (
 	"strings"
 )
 
-func getUserInput() string {
+func getA() int {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 
-	var scanInput string
-	scanInput = scanner.Text()
-	return scanInput
+	input := scanner.Text()
+
+	parts := strings.Fields(input)
+	lastPart := parts[len(parts)-1]
+
+	scanA, err := strconv.Atoi(lastPart)
+	if err != nil {
+		fmt.Println("Error converting input to integer: ", err)
+	}
+	return scanA
 }
 
-func diffieConst(userInput string) []string {
+func diffieConst(userInput string) ([]string, []string, []string, []int, []int) {
 	words := strings.Fields(userInput)
-	var nums []string
+	var nums, p, g []string
+	var intP, intG []int
 
 	for _, word := range words {
 		number, _ := strconv.ParseInt(word, 10, 64)
@@ -27,11 +35,71 @@ func diffieConst(userInput string) []string {
 			nums = append(nums, strconv.Itoa(int(number)))
 		}
 	}
-	return nums
+	for i, value := range nums[:2] {
+		if i == 0 {
+			p = append(p, value)
+		} else if i == 1 {
+			g = append(g, value)
+		}
+	}
+	for _, val := range p {
+		intValP, err := strconv.Atoi(val)
+		if err != nil {
+			fmt.Printf("Error converting string to integer: %v/n", err)
+		}
+		intP = append(intP, intValP)
+	}
+	for _, val := range g {
+		intValG, err := strconv.Atoi(val)
+		if err != nil {
+			fmt.Printf("Error converting string to integer: %v/n", err)
+		}
+		intG = append(intG, intValG)
+	}
+	return nums, p, g, intP, intG
+}
+
+func calcB(intG []int, intP []int, b int) int {
+	g := intG[0]
+	p := intP[0]
+	result := 1
+	base := g % p
+
+	for b > 0 {
+		if b%2 == 1 {
+			result = (result * base) % p
+		}
+		base = (base * base) % p
+		b /= 2
+	}
+	return result
+}
+
+func calcSharedSecret(A int, b int, intP []int) int {
+	p := intP[0]
+	result := 1
+	A = A % p
+
+	for b > 0 {
+		if b%2 == 1 {
+			result = (result * A) % p
+		}
+		A = (A * A) % p
+		b /= 2
+	}
+	return result
 }
 
 func main() {
+	var b int
+	b = 15
 	scanInput := getUserInput()
-	nums := diffieConst(scanInput)
-	fmt.Printf("g=%v and p=%v", nums[0], nums[1])
+	fmt.Println("OK")
+	AResult := getA()
+	_, _, _, intP, intG := diffieConst(scanInput)
+	BResult := calcB(intG, intP, b)
+	SResult := calcSharedSecret(AResult, b, intP)
+	fmt.Println("B is ", BResult)
+	fmt.Println("A is ", AResult)
+	fmt.Println("S is ", SResult)
 }
